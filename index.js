daea1f3a43b2cdc0109da59c16151e3291b543e0
Dontavious Green <dontavious.o.green@gmail.com>
2022-09-16 20:58:09 -0500

    HTML setup Ver.1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('code works!');
+
+
+
+
+
+
+async function ETphoneHome() {
+
+    const SetiSignal = await fetch("https://www.jsontest.com/");
+    const WoWsignal = await SetiSignal.json();
+    console.log(WoWsignal)
+    //const ETprobe = await SetiSignal();
+    console.log(GetAhSignal);
+    // Alien ignored your call
+    if (SetiSignal.ok === false) {
+        console.log("cosmic signals undetected!")
+        return null;
+    }
+
+    return await SetiSignal.json();
+
+}
+
+ETphoneHome();
+
+  //const WoWsignal = await SetiSignal.json();
+   // console.log(json)
+  //  return WoWsignal;
+
