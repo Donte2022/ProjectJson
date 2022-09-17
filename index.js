75e5de44e93a3905dabcc6903a6e945f7e056872
Dontavious Green <dontavious.o.green@gmail.com>
2022-09-16 22:27:37 -0500

    Knocked out the Time and Date  req. Need to convert time to local time

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
 
 
-
-
-/*
-
-async function ETphoneHome() {
-
-    const SetiSignal = await fetch("http://ip.jsontest.com/");
-    const WoWsignal = await SetiSignal.json();
-    console.log(WoWsignal)
-    //const ETprobe = await SetiSignal();
-    //console.log(GetAhSignal);
-    // Alien ignored your call
-    if (SetiSignal.ok === false) {
-        console.log("cosmic signals undetected!")
-        return null;
-    }
-
-    return await SetiSignal.json();
-
-}
-
-ETphoneHome();
-
-  //const WoWsignal = await SetiSignal.json();
-   // console.log(json)
-  //  return WoWsignal;
-
-*/
-
-
 async function ETcallHome() {
 
     const SetiSignal = await fetch("http://ip.jsontest.com/");
     const WoWsignal = await SetiSignal.json();
-    console.log("btton not works")
-    console.log(WoWsignal)
-    const Ufo = document.getElementById("IpMan")
-    const Uap = document.getElementById("HpHitters")
+    const Ufo = document.getElementById("IpMan");
 
     if (SetiSignal.ok === false) {
         console.log("cosmic signals undetected!")
@@ -45,8 +12,90 @@ async function ETcallHome() {
         return null;
 
     } else {
+        console.log(WoWsignal.ip)
         return Ufo.innerHTML = WoWsignal.ip;
     }
 
 }
 
+async function timeTravel() {
+
+    const foresight = await fetch("http://date.jsontest.com");
+    const SpaceWarp = await foresight.json();
+    const Interdimensional = document.getElementById("hardtime");
+
+    if (foresight.ok === false) {
+        console.log("cosmic signals undetected!")
+        console.log(foresight.value)
+        return null;
+
+    } else {
+        console.log(SpaceWarp)
+        //Convert to local time
+        return Interdimensional.innerHTML = SpaceWarp.time + " " +SpaceWarp.date;
+    }
+
+}
+
+/*
+async function ETcallHome() {
+
+    const SetiSignal = await fetch("http://ip.jsontest.com/");
+    const WoWsignal = await SetiSignal.json();
+    const Ufo = document.getElementById("IpMan");
+    const Uap = document.getElementById("HpHitters");
+    const Interdimensional = document.getElementById("hardtime");
+
+    if (SetiSignal.ok === false) {
+        console.log("cosmic signals undetected!")
+        console.log(SetiSignal.value)
+        return null;
+
+    } else {
+        console.log(WoWsignal.ip)
+        return Ufo.innerHTML = WoWsignal.ip;
+    }
+
+}
+
+async function ETcallHome() {
+
+    const SetiSignal = await fetch("http://ip.jsontest.com/");
+    const WoWsignal = await SetiSignal.json();
+    const Ufo = document.getElementById("IpMan");
+    const Uap = document.getElementById("HpHitters");
+    const Interdimensional = document.getElementById("hardtime");
+
+    if (SetiSignal.ok === false) {
+        console.log("cosmic signals undetected!")
+        console.log(SetiSignal.value)
+        return null;
+
+    } else {
+        console.log(WoWsignal.ip)
+        return Ufo.innerHTML = WoWsignal.ip;
+    }
+
+}
+
+async function ETcallHome() {
+
+    const SetiSignal = await fetch("http://ip.jsontest.com/");
+    const WoWsignal = await SetiSignal.json();
+    const Ufo = document.getElementById("IpMan");
+    const Uap = document.getElementById("HpHitters");
+    const Interdimensional = document.getElementById("hardtime");
+
+    if (SetiSignal.ok === false) {
+        console.log("cosmic signals undetected!")
+        console.log(SetiSignal.value)
+        return null;
+
+    } else {
+        console.log(WoWsignal.ip)
+        return Ufo.innerHTML = WoWsignal.ip;
+    }
+
+}
+
+ */
